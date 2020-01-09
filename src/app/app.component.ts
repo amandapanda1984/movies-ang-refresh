4d4f09bb44224c042a5bb66fe75d10256025144a
Amanda Donius <amandakdonius@gmail.com>
2020-01-08 19:51:10 -0600

    added movies-list comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Movie } from './movie';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'movies';
+
+  watched = [
+    new Movie("Ghost Ship", "Horror", 2019),
+    new Movie("Shrek", "Comedy", 2019)
+  ];
+
+  toWatch = [
+    new Movie("The Woman in Black", "Horror", 2019),
+    new Movie("Frozen", "Comedy", 2019)
+  ];
 }
